75ce4800e6c2a35a528b767b16d124f096889071
Julian Scheel <julian@jusst.de>
2020-07-30 12:08:15 +0200

    Add data model classes
    Implement classes which can be serialized to json that describe the
    system and playback state.
    
    Signed-off-by: Julian Scheel <julian@jusst.de>

---
+++ lib/model/metadata.dart
@@ -0,0 +1,17 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'metadata.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class MetaData {
+  final String title;
+  final String artist;
+  final String coverArt;
+  final int duration;
+
+  MetaData({this.title, this.artist, this.coverArt, this.duration});
+
+  factory MetaData.fromJson(Map<String, dynamic> json) =>
+		  _$MetaDataFromJson(json);
+  Map<String, dynamic> toJson() => _$MetaDataToJson(this);
+}
---
+++ lib/model/state.dart
@@ -0,0 +1,33 @@
+import 'package:json_annotation/json_annotation.dart';
+
+import 'metadata.dart';
+
+part 'state.g.dart';
+
+enum SystemState { booting, updating, error, ready }
+
+enum PlaybackState { inactive, paused, playing }
+
+enum BluetoothState { inactive, pairing, connected }
+
+@JsonSerializable(explicitToJson: true, includeIfNull: false)
+class State {
+  final SystemState system;
+  final PlaybackState playback;
+  final int volume;
+  final BluetoothState bluetooth;
+  final MetaData metadata;
+  final int playbackPosition;
+
+  State({
+    this.system,
+    this.playback,
+    this.volume,
+    this.bluetooth,
+    this.metadata,
+    this.playbackPosition,
+  });
+
+  factory State.fromJson(Map<String, dynamic> json) => _$StateFromJson(json);
+  Map<String, dynamic> toJson() => _$StateToJson(this);
+}
