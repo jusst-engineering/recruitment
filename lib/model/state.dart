a05e19ce58346627d90b07c6fd2876ae86792ed4
Julian Scheel <julian@jusst.de>
2025-01-14 11:33:55 +0100

    model/state: Add documentation
    Signed-off-by: Julian Scheel <julian@jusst.de>

--- lib/model/state.dart
+++ lib/model/state.dart
@@ -4,21 +4,74 @@ import 'metadata.dart';
 
 part 'state.g.dart';
 
-enum SystemState { booting, updating, error, ready }
+/// List of core sytem states.
+enum SystemState {
+  /// The system is currently booting.
+  booting,
 
-enum PlaybackState { inactive, paused, playing }
+  /// The system is curently installing an update.
+  updating,
 
-enum BluetoothState { inactive, pairing, connected }
+  /// The system reached an error state.
+  error,
 
+  /// The system is ready for user interaction.
+  ready,
+}
+
+/// List of audio playback states.
+enum PlaybackState {
+  /// The system is inactive and not playing any audio.
+  inactive,
+
+  /// The system has an active audio stream, which is currently paused, so that
+  /// no actual audio data is played out.
+  paused,
+
+  /// The system is currently playing an audio stream.
+  ///
+  /// Check [State.metadata] to obtain more detailed information in this state.
+  playing,
+}
+
+/// List of bluetooth connection states
+enum BluetoothState {
+  /// Bluetooth is inactive and no connection exists.
+  inactive,
+
+  /// Bluetooth is in pairing mode, so that devices can connect.
+  pairing,
+
+  /// A bluetooth connection to another device is established.
+  connected,
+}
+
+/// System state update message.
+///
+/// The message receiver must aggregate the system state to keep track of a
+/// coherent state. Any field is nullable in the messages. Whenever a field has
+/// a value of `null`, the system must keep the last non-`null` value in the
+/// aggreated state.
 @JsonSerializable(explicitToJson: true, includeIfNull: false)
 class State {
   final SystemState? system;
+
+  /// The base syste state.
   final PlaybackState? playback;
+
+  /// Current audio volume given in percent.
   final int? volume;
+
+  /// Current bluetooth connection state.
   final BluetoothState? bluetooth;
+
+  /// Current playback metadata.
   final MetaData? metadata;
+
+  /// Current playback position.
   final int? playbackPosition;
 
+  /// Create a [State] instance with the given fields initialized.
   State({
     this.system,
     this.playback,
